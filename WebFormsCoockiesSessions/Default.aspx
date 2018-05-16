590f84463e524e9c894ae93eb77629b4b7f82cfa
YuriiTrofimenko <tyaa@ukr.net>
2018-05-16 20:10:28 +0300

    init

---
+++ WebFormsCoockiesSessions/Default.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormsCoockiesSessions.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label runat="server" ID="lang"><%= language%></asp:Label>
+    </div>
+    <div>
+        <asp:Button Text="RU" ID="RU" runat="server" OnClick="RU_Click" />
+    </div>
+    <div>
+        <asp:Button Text="Default" ID="Default_lang" runat="server" OnClick="Default_Click" />
+    </div>
+    <div>
+        <asp:Label runat="server" ID="userLabel"><%= currentUserName%></asp:Label>
+    </div>
+        <div>
+            <asp:TextBox ID="login" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button Text="SignIn" ID="SignIn" runat="server" OnClick="SignIn_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormsCoockiesSessions/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsCoockiesSessions
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public string language = "";
+        public List<String> users = new List<string>() { "Vasia", "Petia" };
+        public String currentUserName = "No user";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Создать объект cookie-набора.
+            //читаем куки, если они пришли с клиента
+            HttpCookie cookie = Request.Cookies["Preferences"];
+
+            if (cookie != null)
+            {
+                language = cookie["Language"];
+            }
+            else {
+
+                cookie = new HttpCookie("Preferences");
+                // Установить значение в нем.
+                cookie["Language"] = "English";
+                language = "English";
+                // Добавить в него еще одно значение.
+                cookie["Country"] = "US";
+                //Хранить 1 год
+                cookie.Expires = DateTime.Now.AddYears(1);
+                // Добавить cookie-набор к текущему веб-ответу.
+                Response.Cookies.Add(cookie);
+            }
+
+            if (Session["currentUserName"] != null)
+            {
+                currentUserName = Session["currentUserName"] as String;
+            }
+        }
+
+        protected void RU_Click(object sender, EventArgs e)
+        {
+            HttpCookie cookie = new HttpCookie("Preferences");
+            cookie["Language"] = "Russian";
+            Response.Cookies.Add(cookie);
+            // Добавить в него еще одно значение.
+            cookie["Country"] = "US";
+            //Хранить 1 год
+            cookie.Expires = DateTime.Now.AddYears(1);
+            Response.Redirect(@"/Default.aspx");
+        }
+
+        protected void Default_Click(object sender, EventArgs e)
+        {
+            //удаляем куки, обнуляя время хранения
+            HttpCookie cookie = new HttpCookie("Preferences");
+            cookie.Expires = DateTime.Now.AddDays(-1);
+            Response.Cookies.Add(cookie);
+            Response.Redirect(@"/Default.aspx");
+        }
+
+        protected void SignIn_Click(object sender, EventArgs e)
+        {
+            if (users.Contains(login.Text))
+            {
+                Session["currentUserName"] = login.Text;
+                currentUserName = login.Text;
+            }
+        }
+    }
+}
